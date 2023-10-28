d89fadaac2d0c7f761fbffc542324b2e9cd08a1b
Jacob_Pritchett1 <jake.pritchett21@outlook.com>
2023-10-28 14:06:34 -0500

    Print Function

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -12,13 +12,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
-@app.route('/')
+@app.route('/') #Route to landing page
 def home():
     return render_template('landing.html')
 @app.route('/dashboard') #Only accessed once a user is logged in.
 def dash():
     return render_template('index.html')
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST']) #Login Hidden Route
 def login():
     if request.method == 'POST':
         data = { 'email' : request.form['email']}
@@ -33,11 +33,11 @@ def login():
             return redirect('/dashboard')
     return render_template('login.html')
 
-@app.route('/create_user')
+@app.route('/create_user') #Route to create user form
 def register():
     return render_template('create_user.html')
 
-@app.route('/registration/process', methods=['POST'])
+@app.route('/registration/process', methods=['POST']) #Create User Hidden Route
 def registered():
     is_valid = User.validate_user(request.form)
     if not is_valid:
@@ -54,22 +54,30 @@ def registered():
     User.save(data)
     return redirect("/")
 
-@app.route("/customers")
-def all_user_customers():
+@app.route("/customers") #Route for "My Customers" page
+def all_user_customers(): #This function gets all customers that were created by the user that is currently logged in via session.
     user_id = session.get('user_id')
     if user_id is not None:
         companies = Company.get_user_companies(user_id)
         return render_template('my-customers.html', companies=companies)
     else:
         return redirect('/login')
+@app.route("/customer_report")
+def create_customer_report():
+    user_id = session.get('user_id')
+    if user_id is not None:
+        customers = Company.get_user_companies(user_id)
+        return render_template('customer_report.html',customers=customers)
+    else: 
+        return redirect('login')
 
-@app.route("/new/company") #This displays a form for adding 
+@app.route("/new/company") #This displays a form for adding a company
 def creating_company():
     return render_template("company_creation.html")
 
-@app.route("/new/company/create", methods=['POST'])
-def company_submission():
-    if "user_id" not in session:
+@app.route("/new/company/create", methods=['POST']) #Hidden Route
+def company_submission(): #Function for creating a company
+    if "user_id" not in session: #If user is not validated via session, they are redirected to login
         return redirect('/login')
     if not Company.validate_company(request.form):
         return redirect('/new/company')
@@ -85,14 +93,14 @@ def company_submission():
     }
     Company.create_company(data)
     return redirect("/dashboard")
-@app.route('/edit/<int:id>')
-def edit_post(id):
+@app.route('/edit/<int:id>') #Route to edit company 
+def edit_company(id): #Function that will reroute user if not logged in, this will edit the company
     if "user_id" not in session:
         return redirect('/')
     single_company=Company.get_one(id)
     return render_template("edit_post.html", single_company= single_company,id=id)
 
-@app.route('/edit/<int:id>/finalize', methods=["POST"])
+@app.route('/edit/<int:id>/finalize', methods=["POST"]) #Hidden route for editing the company
 def finalize_edit(id):
     if not Company.validate_post(request.form):
         return redirect('/edit/<int:id>')
@@ -105,7 +113,7 @@ def finalize_edit(id):
     Company.edit_company(data)
     return redirect('/index')
 
-@app.route('/company/delete/<int:id>')
+@app.route('/company/delete/<int:id>') #Route for deleting company
 def delete(id):
     if "user_id" not in session:
         return redirect(url_for('login'))
@@ -113,12 +121,12 @@ def delete(id):
     return redirect('/')
 
 
-@app.route('/logout')
+@app.route('/logout') #Will log the user out
 def logging_out():
     session.clear()
     return redirect('/')
 
-@app.route('/csv', methods=['GET', 'POST'])
+@app.route('/csv', methods=['GET', 'POST']) #Route for CSV Upload and POST route for processing CSV
 def upload_csv():
     if 'file' not in request.files:
         return "No file part"
@@ -131,6 +139,7 @@ def upload_csv():
         df = pd.read_csv(filename)
         html_table = df.to_html(classes='table table-striped', escape=False, index=False)
         return render_template('index.html', html_table=html_table)
+    
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
